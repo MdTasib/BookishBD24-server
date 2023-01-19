0ecbc050be115e7da363817c3a2b728481cc349a
MdTasib <ohidulalam442@gmail.com>
2023-01-19 19:04:37 +0600

    database connected

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const port = process.env.PORT || 5000;
-const { connectToServer } = require("./version1/utils/dbConnected");
-const errorHandler = require("./version1/middlewares/errorHandler");
-const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
 
-// MIDDLEWARES
-app.use(cors());
-app.use(express.json());
-app.use(errorHandler);
+const app = require("./app");
 
-// ROUTES
-
-// DATABASE CONNECTED
-connectToServer(err => {
-	if (!err) {
-		app.listen(process.env.PORT, () => console.log("Port 5000 server running"));
-	} else {
-		console.log("DB DON'T CONNECTED");
-	}
-});
-
-// NOT FOUND ROUTE
-app.all("*", (req, res) => {
-	res.send("Route Not Found");
-});
-
-// ERROR HANDLE
-process.on("unhandledRejection", error => {
-	console.log(error.name, error.message);
-	app.close(() => {
-		process.exit(1);
+// DATABASE CONNECTION
+mongoose
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("Database connection is successfully".green.bold);
 	});
+
+// SERVER RUNING PORT
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+	console.log(`App is running on port ${port}`.yellow.bold);
 });
